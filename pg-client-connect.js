be8657dc889ba6e02a69914db705aadd50d02559
Arien Kock <arien@positor.nl>
2019-04-16 13:50:13 +0200

    Multiple queries step

--- pg-client-connect.js
+++ pg-client-connect.js
@@ -3,8 +3,10 @@ const client = new Client({ connectionString: 'postgresql://postgres:secret@loca
 client.connect()
     .then(() => {
         console.log("Connection to Postgres established!")
-        return client.query('SELECT NOW()')
     })
+    .then(() => client.query('CREATE TABLE IF NOT EXISTS test_table (id serial, test_column varchar(255))'))
+    .then(() => client.query('INSERT INTO test_table (test_column) VALUES (\'foo\')'))
+    .then(() => client.query('SELECT * FROM test_table'))
     .then(res => {
         console.log(res.rows)
         return client.end()
