1e6fa9c0fddeb1710a530f0336dd3fd90b263aa1
Kelley van Evert <kelley@codaisseur.com>
2019-10-15 21:22:19 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
 const loggingMiddleware = (req, res, next) => {
-    const time = new Date().toString()
-    console.log("request received at " + time)
-    res.setHeader("X-Codaisseur-Time", time)
-    next()
-}
+  const time = new Date().toString();
+  console.log("request received at " + time);
+  res.setHeader("X-Codaisseur-Time", time);
+  // Continue on to the next middleware or handler
+  next();
+};
+
 const failRandomlyMiddleware = (req, res, next) => {
-    if (Math.random() < 0.5) {
-        res.status(500).end()
-    } else {
-        next()
-    }
-}
-app.use(loggingMiddleware)
-app.get('/', failRandomlyMiddleware, (req, res) => res.send('Hello'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  if (Math.random() < 0.5) {
+    res.status(500).end();
+  } else {
+    next();
+  }
+};
+
+app.use(loggingMiddleware);
+
+app.get("/", failRandomlyMiddleware, (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
