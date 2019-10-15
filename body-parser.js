88706e6f59241552c42210257823119b1a6ef05e
Kelley van Evert <kelley@codaisseur.com>
2019-10-15 21:19:21 +0200

    Update body-parser.js

--- body-parser.js
+++ body-parser.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 4000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 4000;
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 app.post('/', (req, res) => {
-    console.log(req.body)
+    // Let's look at what's in the body.
+    // Because we've registered the body-parser
+    //  middleware, this should be set and object-like
+    console.log(req.body);
     res.json({
         message: "We received your request body!",
-    })
-})
-app.listen(port, () => console.log("listening on port " + port))
+    });
+});
+
+app.listen(port, () => console.log("listening on port " + port));
