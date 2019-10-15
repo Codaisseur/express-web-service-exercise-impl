0ec1e62f30570b1c3a7620237f7047d388e12c87
Kelley van Evert <kelley@codaisseur.com>
2019-10-15 22:58:00 +0200

    Update pg-client-connect.js

--- pg-client-connect.js
+++ pg-client-connect.js
@@ -1,17 +1,19 @@
-const { Client } = require('pg')
-const client = new Client({ connectionString: 'postgresql://postgres:secret@localhost:5432/postgres' })
+const { Client } = require("pg");
+
+const client = new Client({ connectionString: "postgresql://postgres:secret@localhost:5432/postgres" });
+
 client.connect()
-    .then(() => {
-        console.log("Connection to Postgres established!")
-    })
-    .then(() => client.query('CREATE TABLE IF NOT EXISTS test_table (id serial, test_column varchar(255))'))
-    .then(() => client.query('INSERT INTO test_table (test_column) VALUES (\'foo\')'))
-    .then(() => client.query('SELECT * FROM test_table'))
-    .then(res => {
-        console.log(res.rows)
-        return client.end()
-    })
-    .then(() => {
-        console.log("Connection closed!")
-    })
-    .catch(err => console.error(err))
+  .then(() => {
+    console.log("Connection to Postgres established!")
+  })
+  .then(() => client.query("CREATE TABLE IF NOT EXISTS test_table (id serial, test_column varchar(255))"))
+  .then(() => client.query("INSERT INTO test_table (test_column) VALUES ('foo')"))
+  .then(() => client.query("SELECT * FROM test_table"))
+  .then(res => {
+    console.log(res.rows);
+    return client.end();
+  })
+  .then(() => {
+    console.log("Connection closed!");
+  })
+  .catch(err => console.error(err))
