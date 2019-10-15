c9f1040bc74c7904b904f0df9d80fe4436e5ffea
Kelley van Evert <kelley@codaisseur.com>
2019-10-15 22:55:28 +0200

    Update pg-pool-connect.js

--- pg-pool-connect.js
+++ pg-pool-connect.js
@@ -1,24 +1,26 @@
-const { Pool } = require('pg')
-const pool = new Pool({ connectionString: 'postgresql://postgres:secret@localhost:5432/postgres' })
-pool.on('error', (err, client) => {
-    console.error('error event on pool', err)
+const { Pool } = require("pg");
+
+const pool = new Pool({ connectionString: "postgresql://postgres:secret@localhost:5432/postgres" });
+
+pool.on("error", (err, client) => {
+  console.error("error event on pool", err);
 })
 
 function getDatabaseTime() {
-    pool.connect((err, client, release) => {
+  pool.connect((err, client, release) => {
+    if (err) {
+      console.error("error in connect", err);
+    } else {
+      client.query("SELECT NOW()", (err, res) => {
+        release();
         if (err) {
-            console.error('error in connect', err)
+          console.error("error in query", err);
         } else {
-            client.query('SELECT NOW()', (err, res) => {
-                release()
-                if (err) {
-                    console.error('error in query', err)
-                } else {
-                    console.log(res.rows)
-                }
-            })
+          console.log(res.rows);
         }
-    })
+      });
+    }
+  });
 }
 
-setInterval(getDatabaseTime, 1000)
+setInterval(getDatabaseTime, 1000);
