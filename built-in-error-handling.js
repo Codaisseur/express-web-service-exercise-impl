f859865f6e5d92da5f0eb1ef28e1a3c72c6552df
Kelley van Evert <kelley@codaisseur.com>
2019-10-15 21:09:54 +0200

    Update built-in-error-handling.js

--- built-in-error-handling.js
+++ built-in-error-handling.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
 app.get('/', (req, res, next) => {
     if (Math.random() < 0.5) {
         res.send("ok")
     } else {
-        next("not ok")
+        // Calling next with an argument (of any kind)
+        //  causes Express to treat it as an error
+        // This then causes a 500 error
+        next("not ok");
     }
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
